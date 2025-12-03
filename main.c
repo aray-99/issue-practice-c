676806497b13d5e33eee8c659afd7048da7e012e
aray-99 <yara.ryouta193@mail.kyutech.jp>
2025-12-03 18:22:20 +0900

    改行漏れを修正

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 int main() {
     printf("1 + 1 = %d\n", 1 + 1);
     printf("3 - 1 = %d\n", 3 - 1);
-    // Add multiplication (forgot \n)
-    printf("2 * 2 = %d", 2 * 2);
+    // Add multiplication
+    printf("2 * 2 = %d\n", 2 * 2);
     return 0;
 }
